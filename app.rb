2392e02d050fa8b91cb8d45809e8bda695416656
Mike Ball <mikedball@gmail.com>
2012-02-24 22:45:26 -0500

    Cleaning up app.rb

--- app.rb
+++ app.rb
@@ -23,13 +23,7 @@ end
 
 # Helpers
 helpers do
-  def stylesheets(css_files) 
-    css_files.each do |s|
-      "<link src=\"/stylesheets/foo.css\""
-    end
-  end
-
-  def link_to(url, text=url ,opts={})
+  def link_to(url, text=url, opts={})
     attributes = ""
     opts.each { |key, value| attributes << key.to_s << "=\"" << value << "\" "}
     "<a href=\"#{url}\" #{attributes}>#{text}</a>"
