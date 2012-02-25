7e8960d8561b1c670fff758fd859b70fad1d5be1
Mike Ball <mikedball@gmail.com>
2012-02-24 21:35:37 -0500

    App now serves compiled css

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'rubygems'
 require 'sinatra'
+require 'compass'
+require 'sass'
+#require 'haml'
 
 get '/' do
   erb :index
+end
+
+get '/stylesheets/:name.css' do
+  scss(:"stylesheets/#{params[:name]}")
 end
